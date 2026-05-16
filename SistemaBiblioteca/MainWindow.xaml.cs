bc476d2e799cf1486f8422e19bc320dc195781ca
Danilo Arauz <sist.oarauz1101@uml.edu.ni>
2026-05-16 12:16:35 -0600

    Interfaz grafica de aplicacion de WPF

---
+++ SistemaBiblioteca/MainWindow.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System.Windows;
+
+namespace SistemaBiblioteca
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        // Método para el botón de Libros (Asignado a Josué)
+        private void BtnAbrirLibros_Click(object sender, RoutedEventArgs e)
+        {
+            // Más adelante aquí pondremos: 
+            // VentanaLibros ventana = new VentanaLibros();
+            // ventana.Show();
+
+            MessageBox.Show("Módulo de Gestión de Libros.\n\n(Josué conectará su ventana aquí)", "NEXUS LIBRARY", MessageBoxButton.OK, MessageBoxImage.Information);
+        }
+
+        // Método para el botón de Préstamos (Asignado a Alberto)
+        private void BtnAbrirPrestamos_Click(object sender, RoutedEventArgs e)
+        {
+            // Más adelante aquí pondremos: 
+            // VentanaPrestamos ventana = new VentanaPrestamos();
+            // ventana.Show();
+
+            MessageBox.Show("Módulo de Control de Préstamos.\n\n(Alberto conectará su ventana aquí)", "NEXUS LIBRARY", MessageBoxButton.OK, MessageBoxImage.Information);
+        }
+    }
+}
